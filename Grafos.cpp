f5d859cfb72d2b595af4228430d9258bce5f6349
Arthur <arthur.espinaguiar@gmail.com>
2022-06-28 21:05:31 -0300

    matriz de peso

--- Grafos.cpp
+++ Grafos.cpp
@@ -1,11 +1,94 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <typeinfo>
 
 using namespace std;
 
 ifstream entrada;
+ifstream entrada2;
 
+
+void matrizPeso(){
+
+	//Inicialização de Matrizes
+	int ** matrizPeso;
+	int ** matrizMaxMin;
+
+	//Inicializando e pegando a quantidade dos vertices
+
+	entrada2.open("entrada2.txt");
+	string text;
+	int V;
+	entrada2.seekg(0);
+	getline(entrada2,text);
+	V = stoi(text);
+	entrada2.close();
+
+	//Inicialização e Preenchimento das matrizes com 0
+	matrizPeso = new int*[V];
+	matrizMaxMin = new int*[V];
+ 	
+ 	
+ 	for(int i = 0 ; i < V ; i++ ){
+ 		matrizPeso[i] = new int[V];
+	}
+	
+	for(int i = 0 ; i < V ; i++){
+		for(int j = 0 ; j < V ; j++){
+			matrizPeso[i][j] = 0;					
+		}
+	}
+
+	for(int i = 0 ; i < V ; i++ ){
+ 		matrizMaxMin[i] = new int[3];
+	}
+	
+	for(int i = 0 ; i < V ; i++){
+		for(int j = 0 ; j < 3 ; j++){
+			matrizMaxMin[i][j] = 0;					
+		}
+	}
+	//Preenchimento das Matrizes
+	entrada2.open("entrada2.txt");
+	for(int i = 0 ; i < V ; i++){
+		for(int j = 0 ; getline(entrada2,text); j++){
+			if(j > 0){
+				cout << j << endl;
+				
+
+				if(j > V){
+					matrizMaxMin[i][j - V - 1 ] = stoi(text);
+					cout<< "oi" << endl;
+				}
+				else{
+					matrizPeso[i][j-1] = stoi(text);
+				}
+
+				if(j == (V + 3)){
+					break;
+				}
+			
+			}					
+		}
+	}
+	entrada2.close();
+
+
+	//Print Matrizes
+	for( int i = 0; i < V ; i++){
+		for(int j = 0 ; j < V ; j++){
+			cout << matrizPeso[i][j] << " ";
+		}
+		cout << " " ;
+		for(int j = 0 ; j < 3 ; j++){
+			cout << matrizMaxMin[i][j] << " ";
+		}
+		cout << endl;
+	}
+		cout << endl;
+}
+	
 
 
 
@@ -33,8 +116,11 @@ void matrizAdj(int vertice){
 	}
 	
 	for(int i = 0 ; getline(entrada, text) ; i++ ){
-		if(i > 2){
-			
+		if(i > 1){
+			liga1 = stoi(text.substr(0,text.find(";"))) - 1;
+			liga2 = stoi(text.substr(text.find(";") + 1, text.size())) - 1;
+			matrizAdjacencia[liga1][liga2] = 1;
+			matrizAdjacencia[liga2][liga1] = 1;
 		}
 	} 
 	
@@ -85,7 +171,7 @@ int getVertice(){
 	entrada.seekg(0);
 	getline(entrada,text);
 	vertice = stoi(text);
-	entrada.close()
+	entrada.close();
 	return vertice;
 	
 }
@@ -96,7 +182,7 @@ int getAresta(){
 	int arestas;
 	getline(entrada,text);
 	arestas = stoi(text);
-	entrada.close()
+	entrada.close();
 	return arestas;
 	
 }
@@ -108,15 +194,16 @@ int getAresta(){
  	int arestas = getAresta();
  	
 
-	cout << "Matriz de Adjacencia" << endl;
- 	matrizAdj(vertice);
+	//cout << "Matriz de Adjacencia" << endl;
+ 	//matrizAdj(vertice);
  	
 	cout << endl;
 	cout << endl;
  	
-	cout << "Matriz de Incidencia" << endl;
- 	matrizInc(vertice, arestas);
-		
+	//cout << "Matriz de Incidencia" << endl;
+ 	//matrizInc(vertice, arestas);
+	
+	matrizPeso();
 	
 
 
